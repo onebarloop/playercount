4e9634815287c33feeab430f9b590734dfbb304a
onebarloop <alexanderbayer@posteo.de>
2023-07-29 21:47:00 +0200

    basic testing suite

---
+++ src/components/__tests__/AddPlayer.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import AddPlayer from '../AddPlayer.vue'
+
+describe('AddPlayer', () => {
+  it('renders properly', () => {
+    const wrapper = mount(AddPlayer)
+    expect(wrapper.text()).toContain('Add Players')
+  })
+})
